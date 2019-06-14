9f661505bb2c259e6ce16489cc21565f22478003
Kip Tatum <kip.tatum@cherwell.com>
2019-06-14 10:58:26 -0700

    basic add and subtract working

--- Envelopers/Program.cs
+++ Envelopers/Program.cs
@@ -13,13 +13,20 @@ namespace Envelopers
 		{
 			Envelopes = ProcessFile("envelopes.csv");
 			Intro(Envelopes);
-			Console.WriteLine("\n\nWhat would you like to do?\n" +
-				"1. Add Funds To An Envelope\n" +
-				"2. Delete Funds From An Envelope");
-			var input = Console.ReadLine();
 			
-			Routing(input, Envelopes);
-
+			int input;
+			do
+			{
+				Console.WriteLine("\n\nWhat would you like to do?\n" +
+				"1. Add Funds To An Envelope\n" +
+				"2. Delete Funds From An Envelope\n" +
+				"3. Exit");
+				input = int.Parse(Console.ReadLine());
+				Routing(input, Envelopes);
+				Envelopes = ProcessFile("envelopes.csv");
+				Intro(Envelopes);
+			}
+			while (input != 3);
 		}
 
 		private static List<Envelope> ProcessFile(string filePath)
@@ -34,7 +41,7 @@ namespace Envelopers
 
 		public static void Intro(List<Envelope> env)
 		{
-			
+			Console.Clear();
 			Console.WriteLine("Current Envelope Balances");
 			Console.WriteLine("{0,-30} {1, -10} {2,20}", "Date", "Envelope", "Amount");
 			Console.WriteLine("--------------------------------------------------------------");
@@ -53,22 +60,23 @@ namespace Envelopers
 			}
 		}
 
-		public static void Routing(string choice, List<Envelope> envList)
+		public static void Routing(int choice, List<Envelope> envList)
 		{
+			string envName;
 			switch (choice)
 			{
-				case "1":
+				case 1:
 					Console.WriteLine("Which Envelope would you like to add money to?");
-					string envName = Console.ReadLine();
+					envName = Console.ReadLine();
 					AddMoney(envName, envList);
 					break;
 
-				//case "2":
-				//	DeleteMoney();
-
-
+				case 2:
+					Console.WriteLine("Which Envelope did you spend money out of?");
+					envName = Console.ReadLine();
+					DeleteMoney(envName, envList);
+					break;
 			}
-
 		}
 
 		public static void AddMoney(string env, List<Envelope> envList)
@@ -107,6 +115,55 @@ namespace Envelopers
 			List<String> AddTo = new List<string>();
 			AddTo.Add(sb.ToString());
 			File.AppendAllLines("envelopes.csv", AddTo);
+
+			Console.WriteLine("There is currently {0} in the envelope", saveAmt);
+
+			Console.WriteLine("Press any key to go back to the main screen");
+			Console.ReadKey();
+			
+		}
+
+		public static void DeleteMoney(string env, List<Envelope> envList)
+		{
+			var query =
+				from envs in envList
+				.Where(e => e.Name == env)
+				select envs;
+
+			string envName = query.FirstOrDefault().Name;
+			DateTime date = DateTime.Now;
+
+			foreach (var envelope in query.OrderByDescending(e => e.Date).Take(1))
+			{
+				Console.WriteLine($"You Selected {envelope.Name} and it currently has ${envelope.Amount}");
+			}
+
+			Console.WriteLine("\nHow much did you spend out of this envelope?");
+			var addAmt = Console.ReadLine();
+
+			var query2 =
+				from envs in envList
+				.Where(e => e.Name == env)
+				select envs.Amount;
+
+			double amount = query2.LastOrDefault();
+
+			double saveAmt = amount - double.Parse(addAmt) ;
+
+			StringBuilder sb = new StringBuilder();
+			sb.Append(date);
+			sb.Append(",");
+			sb.Append(envName);
+			sb.Append(",");
+			sb.Append(saveAmt);
+			List<String> AddTo = new List<string>();
+			AddTo.Add(sb.ToString());
+			File.AppendAllLines("envelopes.csv", AddTo);
+
+			Console.WriteLine("There is currenty {0} left in the envelope", saveAmt);
+
+			Console.WriteLine("Press any key to go back to the main screen");
+			Console.ReadKey();
 		}
 	}
 }
